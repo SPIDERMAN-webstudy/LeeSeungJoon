463d2e729cefa28ba72a9ae002bdc14f5e0d58ff
Looma1116 <jksjsmlove2@naver.com>
2022-01-05 17:51:08 +0900

    arranged

--- react-beginner/src/App.js
+++ react-beginner/src/App.js
@@ -3,35 +3,70 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [value, setValue] = useState();
+  const [money, setMoney] = useState();
+  const [index, setIndex] = useState("");
+  const [selectedBool, setSelectedBool] = useState(false);
+  const [refresh, setRefresh] = useState(false);
+  const Changed = (event) => {
+    setValue(event.target.value);
+    console.log("changed");
   };
-  console.log(toDos);
+
+  const Submitted = (event) => {
+    event.preventDefault();
+    setMoney(value);
+  };
+  const Selected = (event) => {
+    setIndex(parseInt(event.target.value));
+    setSelectedBool(true);
+  };
+  useEffect(() => {
+    console.log("loading");
+    setLoading(true);
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => setCoins(json.slice(0, 2000)));
+    console.log("loaded");
+    setLoading(false);
+  }, [refresh]);
   return (
     <div>
-      <h1>My ToDos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="what will you do?"
-        />
-        <button>Go!</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>How many Coins you can buy {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <form onSubmit={Submitted}>
+            <input
+              onChange={Changed}
+              value={parseInt(value)}
+              type="number"
+              placeholder="Your money"
+            ></input>
+            <button>Enter</button>
+          </form>
+          <select onChange={Selected}>
+            <option>--Select your coin--</option>
+            {coins.map((item, index) => (
+              <option value={index} key={index}>
+                {item.name} : {item.quotes.USD.price}
+              </option>
+            ))}
+          </select>
+          <button onClick={() => setRefresh((prev) => (prev = !prev))}>
+            새로고침
+          </button>
+          <hr />
+          <h2>
+            ${money} ={" "}
+            {selectedBool ? money / coins[index].quotes.USD.price : ""}{" "}
+            {selectedBool ? coins[index].symbol : ""}
+          </h2>
+        </div>
+      )}
     </div>
   );
 }
