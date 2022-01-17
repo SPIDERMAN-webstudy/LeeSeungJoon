c5cf924ef672231f18b00782535c9d24fd003969
Looma1116 <jksjsmlove2@naver.com>
2022-01-17 19:04:28 +0900

    Year Filter, UDEMY 70

--- best-react/src/App.js
+++ best-react/src/App.js
@@ -1,31 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
-const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
+const App = () => {
+  const [expenses, setExpense] = useState(DUMMY_EXPENSES);
   // return React.createElement(
   //   'div',
   //   {},
@@ -33,9 +34,9 @@ const App = () => {
   //   React.createElement(Expenses, { items: expenses })
   // );
   const ExpenseDataHander = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpense((prev) => [expense, ...prev]);
   };
+
   return (
     <div>
       <NewExpense onExpenseData={ExpenseDataHander} />
