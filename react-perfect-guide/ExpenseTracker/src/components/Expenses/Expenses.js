49ae182575150a270ec48b6fefc8407d2dcd0e7a
Looma1116 <jksjsmlove2@naver.com>
2022-01-19 19:06:18 +0900

    chapter 6

--- react-perfect-guide/ExpenseTracker/src/components/Expenses/Expenses.js
+++ react-perfect-guide/ExpenseTracker/src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
+import ExpenseChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -12,22 +12,16 @@ const Expenses = (props) => {
     console.log(filteredYear);
   };
 
+  const filteredExpenses = props.items.filter(
+    (value) => value.date.getFullYear() === parseInt(filteredYear)
+  );
+
   return (
     <div>
       <Card className="expenses">
+        <ExpenseChart expenses={filteredExpenses} />
         <ExpensesFilter onFilter={filterChangeHandler} year={filteredYear} />
-        {props.items
-          .filter(
-            (value) => value.date.getFullYear() === parseInt(filteredYear)
-          )
-          .map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
