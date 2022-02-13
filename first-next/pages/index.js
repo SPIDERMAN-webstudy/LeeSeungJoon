fc7b18885fbb7b3c09990510efa4bc8802c155dd
Looma1116 <jksjsmlove2@naver.com>
2022-02-13 16:28:47 +0900

    서버사이드렌더링 하게 만들기 (getServeerSideProps)

--- first-next/pages/index.js
+++ first-next/pages/index.js
@@ -2,19 +2,11 @@ import { responseSymbol } from "next/dist/server/web/spec-compliant/fetch-event"
 import { useState, useEffect } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch("/api/movies")).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>loading...</h4>}
-      {movies.map((movie) => (
+      {results?.map((movie) => (
         <div key={movie.id} className="movie">
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -44,4 +36,15 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch("http://localhost:3000/api/movies")
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
